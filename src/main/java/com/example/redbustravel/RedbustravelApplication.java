6dd97f7b525eb2a352e739d9b98a16c296e17fb8
rohit737363 <chougalerohit73@gmail.com>
2023-03-17 11:07:08 +0530

    codepush

---
+++ src/main/java/com/example/redbustravel/RedbustravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.redbustravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedbustravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedbustravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/redbustravel/RedbustravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redbustravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedbustravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
